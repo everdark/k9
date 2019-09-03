9dcb3ec77303dc36652df8c759f477a4c910d51a
Kyle Chung <kylechun9@gmail.com>
2019-09-03 21:33:01 +0800

    Add hands-on script for pymc3.

---
+++ bayesian/labs/03_pymc3_intro.py
@@ -0,0 +1,141 @@
+"""Explore PyMC3: A probabilistic programming Python package.
+
+Several things to know about PyMC3:
+    1. It implements NUTS as the default sampler (same as Stan)
+    2. Theano is the computing backend
+    3. Interface is purely in Python; model code is compiled to C
+For more information, please refer to the official document:
+    https://docs.pymc.io/
+"""
+
+import numpy as np
+import pandas as pd
+import pymc3 as pm
+from scipy import stats
+
+
+np.random.seed(777)
+
+
+# Test the API.
+with pm.Model() as test_model:
+    # Define a free parameter. (A prior.)
+    y = pm.Normal("y", mu=0, sigma=1)
+
+# Compute log-likelihood.
+print(test_model.logp({"y": 0}))
+
+# It should be equivalent to:
+def dnorm(theta, mean=0, sd=1):
+    """Probability density function of a Normal distribution."""
+    return np.exp(-pow(theta - mean, 2) / (2*pow(sd, 2))) / np.sqrt(2*np.pi*pow(sd, 2))
+
+
+print(np.log(dnorm(0)))
+
+
+# --------------------------------------------------- #
+# Binomial model. (De-generated logistic regression.) #
+# --------------------------------------------------- #
+# We use the example of analytical Bayesian discussed here:
+# https://everdark.github.io/k9/bayesian/bayesian_modeling_explained.nb.html#122_analytical_bayesian
+# Out of 10 coin flips, 6 are heads.
+N = 10
+y = 6
+binom_model = pm.Model()
+with binom_model:
+    # Priors.
+    theta = ... # A Uniform distribution--flat prior.
+
+    # Likelihood of observations.
+    yobs = ... # The observation follows a Binomial distribution.
+
+
+# Do MCMC sampling.
+with binom_model:
+    trace = pm.sample(500, chains=4)
+
+
+# Summary table of the Bayesian estimates.
+print(pm.summary(trace).round(3))
+
+
+# --------------- #
+# Logistic model. #
+# --------------- #
+# Use the example dataset we've explored in the notebook:
+# https://everdark.github.io/k9/bayesian/bayesian_modeling_explained.nb.html#4_bayesian_logistic_regression_using_r
+infile = "data/logit.csv"
+data = pd.read_csv(infile)
+data["b"] = 1  # Add a constant term.
+X = data[["b", "x1", "x2", "x3", "x4"]].values
+y = data["y"].values
+
+# Build the model context.
+logit_model = pm.Model()
+with logit_model:
+    # Priors.
+    beta = ... # Assume a standard Normal for all coefficients.
+
+    # Model output.
+    yhat = ... # sigmoid(logits); logits = dot product of X and beta.
+
+    # Likelihood of observations.
+    yobs = ... # y is binary, following a Bernoulli distribution.
+
+
+# Do MCMC sampling.
+with logit_model:
+    trace = pm.sample(500, chains=4)
+
+print(trace["beta"].shape)
+print(trace["beta"])
+
+# Summary table of the Bayesian estimates.
+print(pm.summary(trace).round(3))
+
+# Trace plot. (Need package `arviz`: pip install arviz.)
+pm.traceplot(trace)
+
+
+# ------------- #
+# Linear model. #
+# ------------- #
+# The error term distribution must be fully specified in order to make the model a probability one.
+# Let's generate some simulated data.
+sigma = 1
+beta = [6, 4]
+size = 100
+
+X = np.random.normal(size=(size, 1))
+X = np.hstack([np.ones_like(X), X])  # Include a constant term.
+e = np.random.normal(size=size, loc=0, scale=sigma)
+y = X.dot(beta) + e
+
+# Build the model context.
+linear_model = pm.Model()
+with linear_model:
+    # Priors.
+    beta = ... # Assume Normality but with a higher s.d. to allow for less information.
+    sigma = ... # Truncated standard Normal with only non-negative values.
+
+    # Model output.
+    yhat = ... # Dot product of X and beta.
+
+    # Likelihood of observations.
+    yobs = # y should follow the error term distribution, with the mean value parameterized by the linear model.
+
+
+# Do MCMC sampling.
+with linear_model:
+    trace = pm.sample(500, chains=4)
+
+print(trace["beta"].shape)
+print(trace["beta"])
+
+# Summary table of the Bayesian estimates.
+print(pm.summary(trace).round(3))
+
+# Trace plot. (Need package `arviz`: pip install arviz.)
+pm.traceplot(trace)
+
