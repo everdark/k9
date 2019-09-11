22f9bc5259f82f471d08d837bdb7a26d1fcb65a1
Kyle Chung <kylechun9@gmail.com>
2019-09-11 15:19:58 +0800

    Specify CRAN repo.

--- programming/r/async/install_packages.R
+++ programming/r/async/install_packages.R
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
 
 requirement_file <- "requirements.txt"
-install.packages(readLines(requirement_file))
+install.packages(readLines(requirement_file), repos="http://cran.rstudio.com")
