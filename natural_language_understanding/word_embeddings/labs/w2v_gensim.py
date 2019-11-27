a06531bf5b9c6dc562cf6a57664a912c84eb73d9
Kyle Chung <kylechun9@gmail.com>
2019-11-27 18:22:55 +0800

    Modify lab script.

--- natural_language_understanding/word_embeddings/labs/w2v_gensim.py
+++ natural_language_understanding/word_embeddings/labs/w2v_gensim.py
@@ -3,7 +3,7 @@ import sentencepiece as spm
 
 # Encode sentences into list of tokens as training input.
 # Here we use our pre-trained sentencepiece model as the tokenizer.
-outfile = "data/enwiki_sents.txt"
+outfile = "data/enwiki_sents_1000.txt"
 sp = spm.SentencePieceProcessor()
 sp.Load("m.model")
 sp_sents = []
@@ -15,7 +15,6 @@ len(sp_sents)
 sp_sents[0]
 
 # Train word2vec.
-gensim_w2v_model_file = "gensim_w2v.model"
 gensim_w2v = Word2Vec(sp_sents, sg=1, size=128, window=3, hs=0, negative=5, min_count=5, workers=4)
 
 # Check some similarity.
@@ -28,3 +27,6 @@ gensim_w2v.wv.most_similar(ws_meta + "man")
 gensim_w2v.wv.most_similar(ws_meta + "taiwan")
 
 gensim_w2v.wv.most_similar(ws_meta + "computer")
+
+gensim_w2v.wv.most_similar(ws_meta + "elephants")
+
--- natural_language_understanding/word_embeddings/labs/w2v_tf.py
+++ natural_language_understanding/word_embeddings/labs/w2v_tf.py
@@ -1,9 +1,7 @@
+"""Labs for training word2vec with tensorflow Keras API."""
 
 import os
-import re
-import string
 
-import jsonlines
 import sentencepiece as spm
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import skipgrams
@@ -12,42 +10,69 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+# Sentence file.
 outfile = "data/enwiki_sents.txt"
-infile = "data/enwiki_texts/AA/wiki_00"
-def parse_wiki_sent(infile):
-  paragraphs = []
-  with jsonlines.open(infile) as jlines:
-    for j in jlines:
-      paragraphs.append(j["text"])  # One paragraph per line.
 
-  # Break by newline.
-  lines = []
-  for p in paragraphs:
-    lines.extend(p.lower().split("\n"))
 
-  # Further break by sentence. We use a naive approach here just for simplicity.
-  sents = []
-  for l in lines:
-    if len(l) >= 20:
-      sents.extend(re.split(r"\. |! |\? ", l))
-
-  return sents
-
-sents = parse_wiki_sent(infile)
-print(len(sents))
-
-for s in sents[:3]:
-  print(s + "\n")
+# Learn vocabulary.
+# Make it small so the training time is manageable.
+spm_args = "--input={}".format(outfile)
+spm_args += " --model_prefix=enwiki"
+spm_args += " --vocab_size=30000"
+spm_args += " --model_type=unigram"
+spm.SentencePieceTrainer.Train(spm_args)
 
 sp = spm.SentencePieceProcessor()
-sp.Load("m.model")
+sp.Load("enwiki.model")
 
 
-vocab_size = len(sp)
-batch_size = 512
-sampling_table = make_sampling_table(vocab_size, sampling_factor=1e-3)
+# Model architecture.
+k = 128  # Embedding dimension.
+
+input_targets = tf.keras.layers.Input(shape=(1,), name="targets")
+input_contexts = tf.keras.layers.Input(shape=(1,), name="contexts")
+
+embeddings = tf.keras.layers.Embedding(len(sp), k, name="word_embeddings")
+target_embeddings = embeddings(input_targets)
+context_embeddings = embeddings(input_contexts)
+
+dots = tf.keras.layers.Dot(axes=-1, name="logits")([target_embeddings, context_embeddings])
+outputs = tf.keras.layers.Activation("sigmoid", name="sigmoid")(dots)
+outputs = tf.keras.layers.Reshape((1,), input_shape=(1, 1))(outputs)
+
+optimizer = tf.keras.optimizers.SGD(lr=.1, decay=1e-5, momentum=.9)
+model = tf.keras.Model(inputs=[input_targets, input_contexts],
+                       outputs=outputs,
+                       name="word2vec")
+model.compile(loss="binary_crossentropy", optimizer=optimizer)
+
+print(model.summary())
 
 
+# In-memory training.
+sampling_table = make_sampling_table(len(sp), sampling_factor=1e-3)
+x = []
+y = []
+with open(outfile, "r", encoding="utf-8") as f:
+  for i, line in enumerate(f):
+    ids = sp.EncodeAsIds(line)
+    pairs, labels = skipgrams(
+      ids, vocabulary_size=len(sp),
+      window_size=3, negative_samples=5,
+      shuffle=True, sampling_table=sampling_table
+    )
+    x.extend(pairs)
+    y.extend(labels)
+x = np.array(x)
+y = np.array(y)
+
+x.shape
+
+model.fit(x=[x[:,0], x[:,1]], y=y, batch_size=512, epochs=5, verbose=1)
+model.fit(x=[x[:,0], x[:,1]], y=y, batch_size=512, initial_epoch=5, epochs=10, verbose=1)
+
+
+# On-disk training.
 def parse_line(text_tensor):
   """Convert a raw text line (in tensor) into skp-gram training examples."""
   ids = sp.EncodeAsIds(text_tensor.numpy())
@@ -73,7 +98,7 @@ def parse_line_map_fn(text_tensor):
 dataset = tf.data.TextLineDataset(outfile)
 dataset = dataset.filter(lambda line: tf.greater(tf.strings.length(line), 20))
 dataset = dataset.flat_map(parse_line_map_fn)
-dataset = dataset.shuffle(buffer_size=10000).repeat(count=2)
+dataset = dataset.shuffle(buffer_size=10000).repeat(None)
 dataset = dataset.batch(batch_size, drop_remainder=True)
 dataset = dataset.prefetch(batch_size)
 
@@ -84,47 +109,31 @@ for x, y in dataset:
   break
 
 
-k = 128
-
-# We separate target and context word indices as two input layers.
-input_targets = tf.keras.layers.Input(shape=(1,), name="targets")
-input_contexts = tf.keras.layers.Input(shape=(1,), name="contexts")
-
-# Word embeddings are looked up separately for target and context words.
-embeddings = tf.keras.layers.Embedding(vocab_size, k, name="word_embeddings")
-target_embeddings = embeddings(input_targets)
-context_embeddings = embeddings(input_contexts)
-
-# Dot-product of the target and context word embeddings with sigmoid activation.
-dots = tf.keras.layers.Dot(axes=-1, name="logits")([target_embeddings, context_embeddings])
-outputs = tf.keras.layers.Activation("sigmoid", name="sigmoid")(dots)
-optimizer = tf.keras.optimizers.SGD(lr=.025, decay=1e-6, momentum=.9)
-model = tf.keras.Model(inputs=[input_targets, input_contexts], outputs=outputs, name="word2vec")
-model.compile(loss="binary_crossentropy", optimizer=optimizer)
-
-print(model.summary())
+# It seems that fit_generator only updates gradient per epoch with dataset api.
+# This will be too slow to train our model
+model.fit_generator(dataset, epochs=10, steps_per_epoch=100, shuffle=True, workers=4, use_multiprocessing=True, verbose=1)
 
 
-x1 = embeddings(x["targets"])
-x2 = embeddings(x["contexts"])
-dot_op = tf.keras.layers.Dot(axes=-1, name="logits")
-act_op = tf.keras.layers.Activation("sigmoid", name="sigmoid")
+# Use train_on_batch instead,
+n_steps = 100000
+losses = []
+for i, (x, y) in enumerate(dataset):
+  if i < n_steps:
+    loss = model.train_on_batch(x, y, reset_metrics=True)
+    if i % 1000 == 0:
+      print("Step {}, loss={}".format(i, loss))#, end="\r")
+      losses.append(loss)
+  else:
+    break
 
-d = dot_op([x1, x2])
-print(d.numpy()[:3,:])
-
-a = act_op(d)
-print(a.numpy()[:3,:])
-
-print((x1.numpy() * x2.numpy()).sum(axis=1)[:3])  # Dot products.
-
-print(tf.sigmoid(d.numpy()[:3]).numpy())  # Activation with sigmoid.
+# check if the learning rate is indeed decayed.
+model.optimizer._decayed_lr("float32").numpy()
 
 
-model.fit_generator(dataset, epochs=1, shuffle=True, workers=6, use_multiprocessing=True, verbose=1)
+
+# Test similarity.
 word_vectors = model.get_layer("word_embeddings").weights[0].numpy()
 print(word_vectors.shape)
-print(word_vectors)
 
 
 def find_similar_words(w, wv, top_k=10):
@@ -139,3 +148,8 @@ def find_similar_words(w, wv, top_k=10):
 find_similar_words("love", wv=word_vectors)
 find_similar_words("girl", wv=word_vectors)
 find_similar_words("computer", wv=word_vectors)
+find_similar_words("elephants", wv=word_vectors)
+find_similar_words("elephant", wv=word_vectors)
+find_similar_words("1", wv=word_vectors)
+find_similar_words("and", wv=word_vectors)
+find_similar_words("or", wv=word_vectors)
