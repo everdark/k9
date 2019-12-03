34868e9690a51a9ed8c0431e31b05f17057c74ec
Kyle Chung <kylechun9@gmail.com>
2019-12-03 20:47:59 +0800

    Add back missing lines. Adjust batch size for mem usage.

--- model_explain/bert.py
+++ model_explain/bert.py
@@ -29,14 +29,16 @@ bert_tokenizer = FullTokenizer(
 # Document longer than 512 words wont be able to be encoded by BERT,
 # since its positional encoding has a hard limit for 512 words.
 # We need to summarize the document into <= 512 tokens, or encode sentence by sentence then pool together.
-maxlen = 512
+maxlen = 256
 
 # Encode text with padding, masking, and segmentation (required by BERT even if we don't use it).
+tok_seq_train = [bert_tokenizer.tokenize(text) for text in newsgroups_train.data]
 wid_seq_train = [bert_tokenizer.convert_tokens_to_ids(toks)[:maxlen] for toks in tok_seq_train]
 wid_seq_train_padded = pad_sequences(wid_seq_train, padding="post", maxlen=maxlen)
 wid_seq_train_mask = (wid_seq_train_padded > 0).astype(int)
 segment_ids_train = np.zeros_like(wid_seq_train_mask)
 
+tok_seq_test = [bert_tokenizer.tokenize(text) for text in newsgroups_test.data]
 wid_seq_test = [bert_tokenizer.convert_tokens_to_ids(toks)[:maxlen] for toks in tok_seq_test]
 wid_seq_test_padded = pad_sequences(wid_seq_test, padding="post", maxlen=maxlen)
 wid_seq_test_mask = (wid_seq_test_padded > 0).astype(int)
@@ -58,7 +60,7 @@ print(bert.summary())
 bert.fit(
   x=[wid_seq_train_padded, wid_seq_train_mask, segment_ids_train],
   y=newsgroups_train.target,
-  batch_size=32, epochs=10,
+  batch_size=8, epochs=30,
   validation_data=([wid_seq_test_padded, wid_seq_test_mask, segment_ids_test], newsgroups_test.target),
   validation_steps=20,
   verbose=1)
