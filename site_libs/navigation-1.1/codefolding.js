b13d8e76e1dba2d13f06966ddc510c478fc492cc
Kyle Chung <kylechun9@gmail.com>
2020-07-08 17:17:28 +0800

    Add new project on imbalanced eval.

--- site_libs/navigation-1.1/codefolding.js
+++ site_libs/navigation-1.1/codefolding.js
@@ -20,23 +20,26 @@ window.initializeCodeFolding = function(show) {
   var rCodeBlocks = $('pre.r, pre.python, pre.bash, pre.sql, pre.cpp, pre.stan, pre.julia');
   rCodeBlocks.each(function() {
 
+    // Respect the knitr chunk option `class.source="fold-show"`
+    var showThisChunk = show || $(this)[0].classList.contains('fold-show');
+
     // create a collapsable div to wrap the code in
     var div = $('<div class="collapse r-code-collapse"></div>');
-    if (show || $(this)[0].classList.contains('fold-show'))
-      div.addClass('in');
+    if (showThisChunk)
+      div.collapse('show');
     var id = 'rcode-643E0F36' + currentIndex++;
     div.attr('id', id);
     $(this).before(div);
     $(this).detach().appendTo(div);
 
     // add a show code button right above
-    var showCodeText = $('<span>' + (show ? 'Hide' : 'Code') + '</span>');
-    var showCodeButton = $('<button type="button" class="btn btn-default btn-xs code-folding-btn pull-right"></button>');
+    var showCodeText = $('<span>' + (showThisChunk ? 'Hide' : 'Code') + '</span>');
+    var showCodeButton = $('<button type="button" class="btn btn-default btn-xs btn-secondary btn-sm code-folding-btn pull-right float-right"></button>');
     showCodeButton.append(showCodeText);
     showCodeButton
         .attr('data-toggle', 'collapse')
         .attr('data-target', '#' + id)
-        .attr('aria-expanded', show)
+        .attr('aria-expanded', showThisChunk)
         .attr('aria-controls', id);
 
     var buttonRow = $('<div class="row"></div>');
