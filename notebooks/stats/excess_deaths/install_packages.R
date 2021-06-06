64addaff3cce9a6279ff0cc28ba0b41d3890d530
Kyle Chung <kylechun9@gmail.com>
2021-06-06 23:51:20 +0800

    feat: complete data explore for predicting features

--- notebooks/stats/excess_deaths/install_packages.R
+++ notebooks/stats/excess_deaths/install_packages.R
@@ -1,22 +1,25 @@
 #!/usr/bin/env Rscript
 
 required_pkgs <- c(
-    "rmarkdown",
-    "metathis",
-    "data.table",
-    "DT",
-    "readODS",
-    "ggplot2",
-    "ggrepel",
-    "ggthemes",
-    "ggforce",
-    "rgeos",
-    "sf",
-    "rnaturalearth",
-    "rnaturalearthdata",
-    "lubridate",
-    "countrycode",
-    "WDI",
-    "agtboost"
+  "rmarkdown",
+  "metathis",
+  "magrittr",
+  "data.table",
+  "DT",
+  "readODS",
+  "readxl",
+  "ggplot2",
+  "ggrepel",
+  "ggthemes",
+  "ggforce",
+  "rgeos",
+  "sf",
+  "rnaturalearth",
+  "rnaturalearthdata",
+  "lubridate",
+  "countrycode",
+  "WDI",
+  "agtboost"
 )
-install.packages(required_pkgs, repos="http://cran.rstudio.com")
+missing_pkgs <- required_pkgs[!(required_pkgs %in% installed.packages()[,"Package"])]
+if ( length(missing_pkgs) ) install.packages(missing_pkgs, repos="http://cran.rstudio.com")
